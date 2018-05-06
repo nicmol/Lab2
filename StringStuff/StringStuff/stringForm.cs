98baba6dee954a7f9e5fe47cb8cb0bff0c9c366a
Nicolette Molitor <molitorn@my.lanecc.edu>
2018-05-06 14:52:42 -0700

    stringStuff working

--- StringStuff/StringStuff/stringForm.cs
+++ StringStuff/StringStuff/stringForm.cs
@@ -17,32 +17,74 @@ namespace String_Stuff
         }
 
         private string SwitchCase(string input)
+       // returns the same string that's passed but with opposite case for character       
         {
             string output = "";
-            return output;
+            foreach (char ch in input)
+            {
+                if (char.IsUpper(ch))
+                    output = output + char.ToLower(ch);
+                else                
+                    output = output + char.ToUpper(ch);                
+            }
+                return output;            
         }
 
         private string Reverse(string input)
+        // reverses the order of the characters in the string       
         {
             string output = "";
+            foreach(char ch in input)
+            {
+                output = ch + output;
+            }
+
             return output;
         }
 
         private string PigLatin(string input)
+        // returns string that starts with the 2nd character to end of string 
+        // appended with 1st character and an "ay"   
         {
             string output = "";
+            if (input[0] == 'a' || input[0] == 'e' || input[0] == 'i' || input[0] == 'o' || input[0] == 'u')
+            {
+                output = input + "yay";
+            }
+            else
+            {
+                output = input.Substring(1);
+                output = output + input[0] + "ay";
+            }            
             return output;
         }
 
         private string ShiftCypher(string input, int charsToShift)
+        // returns a string that translates or codes each character
+        // based on shifting to another character in alphabet
+        // by the charToShift
         {
             string output = "";
+            foreach (char ch in input)
+            {
+                int asciiValue = (int)ch;
+                asciiValue = (((asciiValue - 97) + charsToShift) % 26) + 97;
+                output = output + (char)asciiValue;
+            }
             return output;
         }
 
         private string SubCypher(string input, string charsToSub)
+
         {
             string output = "";
+            foreach (char ch in input)
+            {
+                int asciiValue = (int)ch;
+                int index = asciiValue - 97;
+                char subCharacter = charsToSub[index];
+                output = output + subCharacter;
+            }
             return output;
         }
 
@@ -50,6 +92,10 @@ namespace String_Stuff
         {
             string input = inputTextBox.Text;
             switchCaseTextBox.Text = SwitchCase(input);
+            reverseTextBox.Text = Reverse(input);
+            pigLatinTextBox.Text = PigLatin(input);
+            shiftTextBox.Text = ShiftCypher(input, 2);
+            subTextBox.Text = SubCypher(input, "bcdefghijklmnopqrstuvwxyza");
 
         }
     }
